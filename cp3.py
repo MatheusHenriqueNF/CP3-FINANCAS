908910a9b15d6845eee69e821167923337a7058f
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2024-10-25 15:48:22 -0300

    def 1 feita

--- cp3.py
+++ cp3.py
@@ -3,30 +3,67 @@
 #Estruturas de Dados: Utilize listas para armazenar as transações, onde cada transação é um dicionário contendo tipo (receita ou despesa), valor, categoria, e data. Dicionários podem ser usados para categorizar as transações e calcular totais por categoria.
 
 
-def transacaoEntrada():
-    
-    transacaoLista = {
-        "RECEITA": [],
-        "DESPESA": []
-    }
-    
+def adicionar_transacao():
+    respostaInicial = input("Deseja começar com um valor inicial na sua conta? (sim/não) ")
+
+    if respostaInicial.lower() == 'sim':
+        while True:
+            try:
+                valorInicial = float(input("Qual o valor inicial da sua conta? "))
+                if valorInicial < 0:
+                    print("Valor inicial inválido. Informe um valor positivo.")
+                else:
+                    print(f"O valor inicial da sua conta é: {valorInicial}")
+                    break
+            except ValueError:
+                print("Erro: Você não digitou um número válido.")
+    elif respostaInicial.lower() == 'não':
+        valorInicial = 0
+        print(f"Seu saldo atual é de: {valorInicial}")
+    else:
+        print("Resposta inválida. O programa será encerrado.")
+        return
+
     while True:
-        resposta = input("Deseja adicionar uma nova transacção? (SIM/NÃO): ")
+        try:
+            inserirValor = int(input("Deseja inserir um novo valor: 1 [SIM] ou 2 [NÃO]? "))
 
-        if resposta.upper() == "SIM":
-            
-            entrada = float(input("Digite o valor da transacção: "))
-            tipo = int(input("Digite o tipo da transacção 1 [RECEITA] ou 2 [DESPESA]): "))
-            
-            if tipo == 1:
-                transacaoLista["RECEITA"].append(entrada)
+            if inserirValor == 1:
+                respostaUsuario = int(input("Você deseja inserir uma: 1 [RECEITA] ou 2 [DESPESA]? "))
 
-            print("O valor de R$" + str(entrada) + " foi adicionado a sua conta!")
-        
-        else:
-           break
+                if respostaUsuario == 1:
+                    valor = float(input("Qual o valor da receita? "))
+                    if valor < 0:
+                        print("Valor inválido. Informe um valor positivo.")
+                    else:
+                        valorInicial += valor
+                        print(f"Receita adicionada. Novo saldo: {valorInicial}")
 
-    return transacaoLista
+                elif respostaUsuario == 2:
+                    valor = float(input("Qual o valor da despesa? "))
+                    if valor < 0:
+                        print("Valor inválido. Informe um valor positivo.")
+                    elif valorInicial >= valor:
+                        valorInicial -= valor
+                        print(f"Despesa adicionada. Novo saldo: {valorInicial}")
+                    
+                    else:
+                        print("Saldo insuficiente. Transação recusada!")
+
+                else:
+                    print("Opção inválida. Informe 1 [RECEITA] ou 2 [DESPESA].")
+
+            elif inserirValor == 2:
+                print("Encerrando o programa.")
+                break
+            else:
+                print("Opção inválida. Informe 1 [SIM] ou 2 [NÃO].")
+
+        except ValueError:
+            print("Erro: Você não digitou um número válido.")
+    return valorInicial
+
+
     
 
 def transacaoSaida():
@@ -42,7 +79,7 @@ def categorizarTransacoes():
     pass
 
 def main():
-    valorEntrada = transacaoEntrada()
+    valorTransacao = adicionar_transacao()
 
 if __name__ == "__main__":
     main()
