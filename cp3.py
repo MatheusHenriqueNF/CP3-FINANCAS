deb2a67e38d5228266fb6ee0a671478db22f1553
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2024-10-25 13:36:12 -0300

    def 1

--- cp3.py
+++ cp3.py
@@ -2,10 +2,32 @@
 
 #Estruturas de Dados: Utilize listas para armazenar as transações, onde cada transação é um dicionário contendo tipo (receita ou despesa), valor, categoria, e data. Dicionários podem ser usados para categorizar as transações e calcular totais por categoria.
 
-print("Olá, mundo!")
 
 def transacaoEntrada():
-    pass
+    
+    transacaoLista = {
+        "RECEITA": [],
+        "DESPESA": []
+    }
+    
+    while True:
+        resposta = input("Deseja adicionar uma nova transacção? (SIM/NÃO): ")
+
+        if resposta.upper() == "SIM":
+            
+            entrada = float(input("Digite o valor da transacção: "))
+            tipo = int(input("Digite o tipo da transacção 1 [RECEITA] ou 2 [DESPESA]): "))
+            
+            if tipo == 1:
+                transacaoLista["RECEITA"].append(entrada)
+
+            print("O valor de R$" + str(entrada) + " foi adicionado a sua conta!")
+        
+        else:
+           break
+
+    return transacaoLista
+    
 
 def transacaoSaida():
     pass
@@ -20,7 +42,7 @@ def categorizarTransacoes():
     pass
 
 def main():
-    pass
+    valorEntrada = transacaoEntrada()
 
 if __name__ == "__main__":
     main()
